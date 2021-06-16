a37ae58f1b1c4b2404f4735a074fc48daa2f8ae8
Aliwoto <aminnimaj@gmail.com>
2021-06-16 16:51:49 +0000

    use super() to call another constructors
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- src/App.java
+++ src/App.java
@@ -1,12 +1,27 @@
-public class App {
-    public static void main(String[] args) throws SomeException {
+import javax.swing.JOptionPane;
 
-		throw new SomeException();
-        System.out.println("Hello, World!");
+public class App {
+    public static void main(String[] args) throws Exception {
+		if (args == null) {
+			System.out.println("no arguments are provided!");
+			return;
+		} else if (args.length == 0) {
+			JOptionPane.showMessageDialog(null, "Onii-chan!");
+			throw new SomeException("tets");
+		}
+		var str = "args: ";
+		for (var current : args) {
+			str += "\n" + current;
+		}
+
+        System.out.println("Hello, World!" + str);
+		
     }
 
-	public class SomeException extends Exception {
-		
+	public static class SomeException extends Exception {
+		public SomeException(String text) {
+			super("A new exception is here waiting for ya: " + text);
+		}
 	}
 }
 
